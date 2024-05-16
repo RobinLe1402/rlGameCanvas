e341bb39e52bfacaa5af206071f10b70fa010d59
RobinLe <web@robinle.de>
2024-05-16 23:03:18 +0200

    XP bugfix: erroneous calls to MultiByteToWideChar

--- src/Windows.cpp
+++ src/Windows.cpp
@@ -6,12 +6,12 @@ namespace rlGameCanvasLib
 	std::wstring UTF8toWindowsUnicode(const U8Char *szUTF8)
 	{
 		const char *szAsChar = reinterpret_cast<const char *>(szUTF8);
-		const auto iLen = MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, szAsChar, -1, nullptr, 0);
+		const auto iLen = MultiByteToWideChar(CP_UTF8, 0, szAsChar, -1, nullptr, 0);
 		if (iLen == 0)
 			return std::wstring{};
 
 		std::wstring sResult(iLen - 1, '\0');
-		MultiByteToWideChar(CP_UTF8, MB_PRECOMPOSED, szAsChar, -1, sResult.data(),
+		MultiByteToWideChar(CP_UTF8, 0, szAsChar, -1, sResult.data(),
 			int(sResult.length() + 1));
 
 		return sResult;
